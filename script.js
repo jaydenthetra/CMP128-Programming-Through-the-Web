716f9d494c28a26e05f6083ddafa3923620e8fc8
jay <125235787+jaydenthetra@users.noreply.github.com>
2025-03-03 17:45:24 -0500

    Update script.js
    added the random quote thingy

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 let quotes = {""We must accept finite disappointment, but never lose infinite hope." ~ Martin Luther King, Jr.", ""A man who wants to lead the orchestra must turn his back on the crowd." ~ Max Lucado", ""Trust in yourself. Your perceptions are often far more accurate than you are willing to believe." ~ Claudia Black",""Love , creativity, and opportunity is in the air. Take it in and do your thing." ~ Lenny Kravitz",};
-let quotes = fruits[0]; // first quote pal
+let quotes = quotes[0]; // first quote pal
