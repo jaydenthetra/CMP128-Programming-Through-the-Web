de9eeddbb4eaabc6aa81175d062acfed683cdbb1
jay <125235787+jaydenthetra@users.noreply.github.com>
2025-03-06 16:10:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let quotes = {""We must accept finite disappointment, but never lose infinite hope." ~ Martin Luther King, Jr.", ""A man who wants to lead the orchestra must turn his back on the crowd." ~ Max Lucado", ""Trust in yourself. Your perceptions are often far more accurate than you are willing to believe." ~ Claudia Black",""Love , creativity, and opportunity is in the air. Take it in and do your thing." ~ Lenny Kravitz",};
 let quotes = quotes[0]; // first quote pal
-function printfirstfruit()
+function printfirstquote()
 {
   let randomnumber= Math.Floor(Math.random()*quotes.length);
 console.log(quotes[0]);
   
 }
-printfirstfruit();
+printfirstquote();
